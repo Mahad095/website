4c42761a6f087dae287ebd4f8e55790f55735d6b
Mahad Hameed <k200338@nu.edu.pk>
2022-04-05 16:12:27 +0500

    Changes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import './index.css'
 export default function Home() {
   return (
-    <div>Hello Home</div>
+      <React.Fragment>
+        <div className="navbar">
+            <a href="" className="navItems">Home</a>
+            <a href="" className="navItems">Projects</a>
+            <a href="" className="navItems">About me</a>
+            <a href="" className="navItems">Contact</a>
+        </div>
+      </React.Fragment>
   )
 }
